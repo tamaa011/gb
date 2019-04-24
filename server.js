db29fbc3a33d6f1cb6578ea9886d8f5ea1f8b429
mohamed tamaa <mtamaaa@gmail.com>
2019-04-25 00:55:31 +0200

    Sdf

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.port || 3001;
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3001;
 
 
 const server = http.createServer(app);
