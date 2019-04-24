8fc44e7246bddd5b3425fc9fd965afd000976d2b
mohamed tamaa <mtamaaa@gmail.com>
2019-04-25 00:52:07 +0200

    g

--- app.js
+++ app.js
@@ -8,8 +8,6 @@
 
  const usersRoutes = require('./api/routes/users');
  const hallsRoutes = require('./api/routes/halls');
- const port = process.env.PORT || 4000;
-
  mongoose.connect(
   //  "mongodb+srv://GroomAndBride:" + process.env.MONGO_ATLAS_PW + "@cluster0-jwktt.mongodb.net/test?retryWrites=true",
   "mongodb+srv://GroomAndBride:123123123@groomandbridecluster-jwktt.mongodb.net/test?retryWrites=true",
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.port || 3001
+const port = process.env.port || 3001;
+const port = process.env.PORT || 4000;
+
 
 const server = http.createServer(app);
 
