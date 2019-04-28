4321dc25a25eb3f8bf45bb58ee6cfad75fc43f4e
mohamed tamaa <mtamaaa@gmail.com>
2019-04-29 00:29:18 +0200

    neeew

--- api/models/hall.js
+++ api/models/hall.js
@@ -5,6 +5,11 @@ const hallSchema = mongoose.Schema({
     hallName: { type: String, required: true },
     hallAdress: { type: String, required: true },
     hallCategory: { type: String, required: true },
+    hallDescription: { type: String, required: true },
+    hallPrice: { type: String, required: true },
+    hallLocationLong: { type: String, required: true },
+    hallLocationLat: { type: String, required: true },
+    hallSpecialOffers: { type: String},
     hallImage: []
 });
 
