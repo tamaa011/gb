adc148728ebb0c234a714112186c63336b80c0a9
mohamed tamaa <mtamaaa@gmail.com>
2019-04-29 23:36:37 +0200

    sdfs

--- api/routes/halls.js
+++ api/routes/halls.js
@@ -11,7 +11,7 @@ const checkAuth = require('../middleware/check-auth');
 //--------------------------------------------------------------------------------------
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, "./uploads/");
+        cb(null, "./uploads");
     },
     filename: function(req, file, cb) {
         cb(null, Date.now() + file.originalname);
