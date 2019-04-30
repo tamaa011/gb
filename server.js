d4dd980d24e2e539354819faf7d513d174df6c63
mohamed tamaa <mtamaaa@gmail.com>
2019-04-30 02:01:52 +0200

    sdsd

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 3001;
 
 
 const server = http.createServer(app);
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/uploads'); // load the single view file (angular will handle the page changes on the front-end)
+});
 
 server.listen(port);
 
