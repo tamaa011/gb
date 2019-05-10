e090621d881c1976152e7a23b98d926db1154e06
mohamed tamaa <mtamaaa@gmail.com>
2019-05-10 17:25:25 +0200

    adf

---
+++ api/routes/halls.js
@@ -0,0 +1,133 @@
+const express = require("express");
+const router = express.Router();
+const hall = require('../models/hall');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const fs = require('fs');
+
+const checkAuth = require('../middleware/check-auth');
+
+// uploading images..
+//--------------------------------------------------------------------------------------
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "./uploads/");
+    },
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + file.originalname);
+    }
+});
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg'){
+        cb(null , true);
+    }else{
+        cb(Error('image is not valid'), false);
+    }
+};
+const upload = multer({storage: storage, limits: {
+    fileSize: 1024 * 1024 * 30
+},
+fileFilter: fileFilter
+});
+
+var result = new Array()
+router.post('/', upload.array('hallImage', 6), (req, res, next) => { 
+    result = []; // insert new hall into ddatabase with images limited to 6
+    for(var i = 0; i < req.files.length; i++){
+        result[i] = req.files[i].filename // this was path .. changed to file name for ignoring 'uploads' in url
+    } 
+
+    const newHall = new hall({
+        _id: new mongoose.Types.ObjectId(),
+        hallName: req.body.hallName,
+        hallAdress: req.body.hallAdress,
+        hallCategory: req.body.hallCategory,
+        hallDescription: req.body.hallDescription,
+        hallPrice: req.body.hallPrice,
+        hallLocationLong: req.body.hallLocationLong,
+        hallLocationLat: req.body.hallLocationLat,
+        hallSpecialOffers: req.body.hallSpecialOffers,
+        hallPhoneNumber: req.body.hallPhoneNumber,
+        hallImage: result
+    });
+    newHall.save().then(result => {
+        res.status(200).json({
+            message: 'Hall Saved Successfully',
+            hall: newHall
+        });
+    }).catch(error => {
+        console.log(error);
+        res.status(500).json({error: error});
+    });
+});
+
+
+// get request
+//------------------------------------------------------------------------------------------
+router.get('/', (req, res, next) => { // get all halls
+    hall.find().select("_id hallName hallAdress hallCategory hallImage hallDescription hallPrice hallLocationLong hallLocationLat hallSpecialOffers hallPhoneNumber")
+    .exec().then(allHalls => {
+        console.log(allHalls);
+        if (allHalls.length >= 0){
+            res.status(200).json(allHalls);
+        }else {
+            res.status(404).json({message: 'No halls found'});
+        }
+    }).catch(error => {
+        console.log(error);
+        res.status(500).json({error: error});
+    });
+});
+
+router.get('/:HallID', (req, res, next) => { // get hall info by id
+    const id = req.params.HallID;
+    hall.findById(id).select("_id hallName hallAdress hallCategory hallImage hallDescription hallPrice hallLocationLong hallLocationLat hallSpecialOffers hallPhoneNumber").exec().then(doc => {
+        console.log("from databse ",doc);
+        if (doc){
+           res.status(200).json({ doc });
+        }else {
+            res.status(404).json({message: 'Not valid hall id'});
+        }
+
+    }).catch(error => {
+        console.log(error);
+        res.status(500).json({error: error});
+    });
+});
+
+
+// delete request
+//--------------------------------------------------------------------------------------------------
+router.delete('/:HallID', (req, res, next) => { // delete hall by id
+    const id = req.params.HallID;
+ 
+    hall.findById(id).exec().then(doc => {  // get hall data from database by id
+             if (doc){ // if hall data exsist
+                 hall.deleteOne({ _id: id }).exec().then(result => { // delete hall data from database
+ 
+                     for(var i = 0; i < doc.hallImage.length; i++){ // loop on hall images 
+                         fs.unlink('./uploads/'+doc.hallImage[i], (error) =>{ // delete each image from uploads folder
+          
+                         });
+                     } // loop end
+                     
+                     return res.status(200).json({
+                         message: 'hall deleted with its images successfully'
+                     });
+ 
+                 }).catch(error => {
+                     console.log(error);
+                     res.status(404).json({error: error});
+                 });  
+             }else {
+               return res.status(404).json({message: 'Not found'});
+             }
+     }).catch(error => {
+         console.log(error);
+         res.status(500).json({error: error});
+     });
+ });
+
+
+
+module.exports = router;
---
+++ api/routes/users.js
@@ -0,0 +1,154 @@
+
+const express = require("express");
+const router = express.Router();
+const User = require('../models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// sign up user 
+//------------------------------------------------------------------------------------------
+router.post('/signup', (req, res, next) => { // sign up new user and check if exist first
+    User.find({ userEmail: req.body.userEmail }).exec().then(user => {
+        if (user.length >= 1){
+            return res.status(409).json({
+                error: 'Mail exists'
+            });
+        }else {
+            
+            bcrypt.hash(req.body.userPassword, 10, (err, hash) => {
+                if (err){
+                    return res.status(500).json({
+                        error: err
+                    });
+                }else {
+                    const user = new User({
+                        _id: new mongoose.Types.ObjectId(),
+                        userName: req.body.userName,
+                        userEmail: req.body.userEmail,
+                        userPassword:  hash
+                    });
+                    user.save().then(reuslt => {
+                        res.status(200).json({
+                            message: 'User sign up successfully',
+                            user: user 
+                        });
+                    }).catch(error => {
+                        console.log(error);
+                        res.status(500).json({error: error});
+                    });
+                }
+            }); 
+
+        }
+    });
+
+});
+
+
+// sign in user 
+//------------------------------------------------------------------------------------------
+router.post('/signin', (req, res, next) => {
+   User.find({ userEmail: req.body.userEmail }).exec().then(user => {
+    if (user.length < 1) {
+        return res.status(401).json({
+            error: 'Auth failed'
+        });
+    }
+    
+    bcrypt.compare(req.body.userPassword, user[0].userPassword, (err, result) => {
+        if (err){
+            return res.status(401).json({
+                error: 'Auth failed'
+            });
+        }
+
+        if (result){
+            const token = jwt.sign(
+                {
+                    userEmail: user[0].userEmail,
+                    userId: user[0]._id
+                }, 
+                'tamaaGamedAwe',
+                {
+                   expiresIn: "1h"
+                });
+
+            return res.status(200).json({
+                message: 'Auth successfull',
+                user: user,
+                token: token
+            }); 
+        }
+
+        res.status(401).json({
+            error: 'Auth failed'
+        });
+
+    });
+
+   }).catch(error => {
+        console.log(error);
+        res.status(500).json({error: error});
+   });
+
+});
+
+
+
+
+// get request
+//------------------------------------------------------------------------------------------
+router.get('/', (req, res, next) => { // get all users we have on database
+    User.find().select("_id userName userEmail userPassword")
+    .exec().then(allUsers => {
+        if (allUsers.length >= 0){
+            res.status(200).json(allUsers);
+        }else {
+            res.status(404).json({error: 'No Users found'});
+        }
+    }).catch(error => {
+        console.log(error);
+        res.status(500).json({error: error});
+    });
+});
+
+router.get('/:userID', (req, res, next) => { // get specific user information by user id
+    const id = req.params.userID;
+    User.findById(id).select("_id userName userEmail userPassword").exec().then(user => {
+        if (user){
+           res.status(200).json({ user });
+        }else {
+            res.status(404).json({error: 'Not valid user id'});
+        }
+
+    }).catch(error => {
+        console.log(error);
+        res.status(500).json({error: error});
+    });
+});
+
+// delete request
+//--------------------------------------------------------------------------------------------------
+router.delete('/:userID', (req, res, nect) => {  // delete user from database by id
+    const id = req.params.userID;
+     User.remove({ _id: id }).exec().then(result => {
+         res.status(200).json({
+             message: 'User Deleted'
+         });
+     }).catch(error => {
+         console.log(error);
+         res.status(500).json({error: error});
+     }); 
+ });
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
