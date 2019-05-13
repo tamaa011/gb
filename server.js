d958c82314caf496a591925567e5c40aa1f94f2b
ebra192 <ebrhimmoussa17@gmail.com>
2019-05-13 23:51:17 +0200

    modify

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const port = process.env.PORT || 3001;
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
-    console.log(`sever listens on port${port}`);
+server.listen(port)
 
-});
 
