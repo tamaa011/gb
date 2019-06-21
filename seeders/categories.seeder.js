54ff04b3793302851a294601ed9dde6a4a17ec4e
ebra192 <ebrhimmoussa17@gmail.com>
2019-06-21 14:35:57 +0200

    fixed id in seeder

--- seeders/categories.seeder.js
+++ seeders/categories.seeder.js
@@ -1,22 +1,29 @@
 var Seeder = require('mongoose-data-seed').Seeder;
 const category = require('../api/models/category');
+const mongoose = require('mongoose');
 
 var data = [{
+  _id: mongoose.Types.ObjectId('5d0cbfc9a758321414bf9871'),
   name: "Hotel"
 },
 {
+  _id: mongoose.Types.ObjectId('5d0cbfc9a758321414bf9872'),
   name: "Club"
 },
 {
+  _id: mongoose.Types.ObjectId('5d0cbfc9a758321414bf9873'),
   name: "Yacht"
 },
 {
+  _id: mongoose.Types.ObjectId('5d0cbfc9a758321414bf9874'),
   name: "Villa"
 },
 {
+  _id: mongoose.Types.ObjectId('5d0cbfc9a758321414bf9875'),
   name: "Open area"
 },
 {
+  _id: mongoose.Types.ObjectId('5d0cbfc9a758321414bf9876'),
   name: "Individual"
 }
 ];
