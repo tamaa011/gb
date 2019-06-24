0b95df5bda3e406b3daee83abc05a26790f167d2
ebra192 <ebrhimmoussa17@gmail.com>
2019-06-24 21:55:47 +0200

    firebase setup again

--- config/initThirdParty.js
+++ config/initThirdParty.js
@@ -1,7 +1,7 @@
 
 const admin = require("firebase-admin");
 const config = require("../config/config.json")
-const serviceAccount = require("./../groomandbride-57ebd-firebase-adminsdk-u7ma0-46158fe88d.json");
+const serviceAccount = require("./../groom-and-bride-app-firebase-adminsdk-k5zo1-5961c22068.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
